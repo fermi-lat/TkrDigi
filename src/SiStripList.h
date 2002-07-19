476e0d435a52eaa8a96a462795c946f47d877d1a
burnett <>
2002-07-19 16:01:07 +0000

    tagging v1r3

--- src/SiStripList.h
+++ src/SiStripList.h
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/TkrDigi/src/SiStripList.h,v 1.1.1.1 2002/05/26 17:17:19 burnett Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/TkrDigi/src/SiStripList.h,v 1.2 2002/06/20 22:23:38 burnett Exp $
 
 #ifndef SiStripList_H
 #define SiStripList_H
@@ -13,26 +13,26 @@ public:
     
     ~SiStripList();
     
-   
+    
     void score(const HepPoint3D& o, const HepPoint3D& p, float eLoss);
-
+    
     void clear();
-
+    
     /// add noise and remove hits below threshold
     /**
-      *  @param noise_sigma noise rms in MeV
-      *  @param occupancy  fraction of time a cell is occupied
-      *  @param theshold   minimium energy deposit (MeV) that results in a latch
-      */
-
+    *  @param noise_sigma noise rms in MeV
+    *  @param occupancy  fraction of time a cell is occupied
+    *  @param theshold   minimium energy deposit (MeV) that results in a latch
+    */
+    
     void addNoise(double noise_sigma, double occupancy, double threshold);
-
+    
     /// Strip data declaration
     class Strip 
     {
     public:
-	
-		// constructor
+        
+        // constructor
         Strip (int index = -1, double energy = 0, bool noise=false)
             :m_index(index), m_energy(energy), m_noise(noise)
         {}
@@ -55,10 +55,10 @@ public:
         
     };
     
-   
+    
     /// for access to the service
     static void initialize(IGlastDetSvc* detsvc);
-
+    
     typedef std::vector < Strip > StripList;            // list of hit strips
     typedef StripList::const_iterator const_iterator;   // iterator
     typedef StripList::iterator iterator;   
@@ -70,26 +70,26 @@ public:
     int size () const { return m_strips.size(); }
     bool empty() const { return size()==0; }
     
-
+    
     static IGlastDetSvc* s_detsvc;
-
+    
     // compute local coordinate from strip id
     static double calculateBin (unsigned int ix);   
-
+    
     static unsigned int stripId(double x);
     void addStrip(unsigned int ix, float dE);
-
-        
-        // access
-        iterator    begin () { return m_strips.begin(); }
-        iterator    end () { return m_strips.end(); }
-        iterator    insert (iterator it, const Strip& x) 
-        { return m_strips.insert(it, x); }
-        iterator    erase (iterator it) { return m_strips.erase(it); }
-        void        push_back (const Strip& x) { m_strips.push_back(x); }
-        
-        StripList   m_strips;
-   
+    
+    
+    // access
+    iterator    begin () { return m_strips.begin(); }
+    iterator    end () { return m_strips.end(); }
+    iterator    insert (iterator it, const Strip& x) 
+    { return m_strips.insert(it, x); }
+    iterator    erase (iterator it) { return m_strips.erase(it); }
+    void        push_back (const Strip& x) { m_strips.push_back(x); }
+    
+    StripList   m_strips;
+    
     /// number of silicon dies across a single layer
     static unsigned int n_si_dies ();
     
@@ -121,9 +121,9 @@ public:
     /// width of dead area, aka guarg_ring
     static double guard_ring ();
     static double panel_width(); 
-
+    
 private:
-
+        
     static double s_dice_width;
     static double s_ssd_gap;
     static double s_ladder_gap;
@@ -131,11 +131,8 @@ private:
     static double s_panel_width;
     static unsigned int s_stripPerWafer;
     static unsigned int s_n_si_dies;
-
+        
 };
-
-
-
 
 
 
