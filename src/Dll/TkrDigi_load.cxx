756a66edd209a1e1a6fd706dc8b9bffa61e89b8a
kyoung <>
2002-10-08 22:18:53 +0000

    Add TkrDigiRandom tool

--- src/Dll/TkrDigi_load.cxx
+++ src/Dll/TkrDigi_load.cxx
@@ -3,13 +3,13 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/GlastDigi/src/Dll/GlastDigi_load.cxx,v 1.5 2002/05/23 18:17:30 heather Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/TkrDigi/src/Dll/TkrDigi_load.cxx,v 1.1.1.1 2002/05/26 17:17:19 burnett Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 DECLARE_FACTORY_ENTRIES(TkrDigi) {
     DECLARE_ALGORITHM( TkrSimpleDigiAlg );
-
+    DECLARE_TOOL( TkrDigiRandom );
 } 
 
