6b27972d11a37afd7ec9b2a5ba9dc1ae4cf7c230
lsrea <>
2003-01-27 00:39:27 +0000

    added call to alignment service

--- src/TkrSimpleDigiAlg.cxx
+++ src/TkrSimpleDigiAlg.cxx
@@ -1,5 +1,5 @@
 // File and Version Information:
-//      $Header: /nfs/slac/g/glast/ground/cvs/TkrDigi/src/TkrSimpleDigiAlg.cxx,v 1.18 2002/12/06 23:58:45 lsrea Exp $
+//      $Header: /nfs/slac/g/glast/ground/cvs/TkrDigi/src/TkrSimpleDigiAlg.cxx,v 1.19 2003/01/06 19:48:28 lsrea Exp $
 //
 // Description:
 //      TkrSimpleDigiAlg provides an example of a Gaudi algorithm.  
@@ -30,6 +30,7 @@
 
 #include "TkrUtil/ITkrFailureModeSvc.h"
 #include "TkrUtil/ITkrBadStripsSvc.h"
+#include "TkrUtil/ITkrAlignmentSvc.h"
 
 #include "idents/VolumeIdentifier.h"
 #include "idents/TowerId.h"
@@ -61,7 +62,7 @@
 *
 * @author T. Burnett
 *
-* $Header: /nfs/slac/g/glast/ground/cvs/TkrDigi/src/TkrSimpleDigiAlg.cxx,v 1.18 2002/12/06 23:58:45 lsrea Exp $  
+* $Header: /nfs/slac/g/glast/ground/cvs/TkrDigi/src/TkrSimpleDigiAlg.cxx,v 1.19 2003/01/06 19:48:28 lsrea Exp $  
 */
 
 class TkrSimpleDigiAlg : public Algorithm {
@@ -97,6 +98,7 @@ private:
 
     ITkrFailureModeSvc * m_fsv;
     ITkrBadStripsSvc   * m_bsv;
+    ITkrAlignmentSvc   * m_asv;
     
     SiLayerList m_layers;
     
@@ -156,6 +158,13 @@ StatusCode TkrSimpleDigiAlg::initialize(){
     m_fsv=0;
     if( service( "TkrFailureModeSvc", m_fsv).isFailure() ) {
         log << MSG::INFO << "Couldn't set up TkrFailureModeSvc" << endreq;
+        log << MSG::INFO << "Will assume it is not required"    << endreq;
+    }
+
+    // Get the alignment service 
+    m_asv=0;
+    if( service( "TkrAlignmentSvc", m_asv).isFailure() ) {
+        log << MSG::INFO << "Couldn't set up TkrAlignmentSvc" << endreq;
         log << MSG::INFO << "Will assume it is not required"    << endreq;
     }
 
@@ -435,10 +444,13 @@ void TkrSimpleDigiAlg::createSiHits(const Event::McPositionHitVector& hits)
         static double waferOffset = 0.5*(SiStripList::n_si_dies() - 1);
         
         int ladder = id[7], wafer=id[8];
-        Hep3Vector 
-            offset( (ladder-waferOffset)*ladder_pitch, (wafer-waferOffset)*ssd_pitch, 0),
+        HepVector3D 
+            offset( (ladder-waferOffset)*ladder_pitch, (wafer-waferOffset)*ssd_pitch, 0);
+        HepPoint3D
             entry(hit.entryPoint()+offset),
-            exit( hit.exitPoint()+offset);       
+            exit( hit.exitPoint()+offset);
+        // move hit by alignment constants
+        if( m_asv && m_asv->alignSim()) m_asv->moveMCHit(id, entry, exit);
         
         //now truncate the id to the plane.
         idents::VolumeIdentifier planeId;
@@ -457,3 +469,30 @@ void TkrSimpleDigiAlg::clear()
     m_SiMap.clear();
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
