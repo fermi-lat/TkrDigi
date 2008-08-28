452f90fbc0d11359bd5560ad431d30456ba17dc4
ecephas <>
2008-08-28 20:21:21 +0000

    Added the following libraries:
       env.Tool('facilitiesLib')
       env.Tool('EventLib')
       env.Tool('GuiSvcLib')
       env.Tool('TkrUtilLib')

--- TkrDigiLib.py
+++ TkrDigiLib.py
@@ -1,7 +1,10 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/TkrDigi/TkrDigiLib.py,v 1.1 2008/07/09 21:13:42 glastrm Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/TkrDigi/TkrDigiLib.py,v 1.1 2008/08/15 21:42:37 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['TkrDigi'])
-
+    env.Tool('facilitiesLib')
+    env.Tool('EventLib')
+    env.Tool('GuiSvcLib')
+    env.Tool('TkrUtilLib')
 def exists(env):
     return 1;
