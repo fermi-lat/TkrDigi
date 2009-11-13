ab9d4225691af074bbd92b7f7f033a5dab23b0cc
jrb <>
2009-11-13 00:47:32 +0000

    Updates for SCons

--- TkrDigiLib.py
+++ TkrDigiLib.py
@@ -1,10 +1,12 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/TkrDigi/TkrDigiLib.py,v 1.1 2008/08/15 21:42:37 ecephas Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/TkrDigi/TkrDigiLib.py,v 1.2 2008/08/28 20:21:21 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['TkrDigi'])
     env.Tool('facilitiesLib')
     env.Tool('EventLib')
-    env.Tool('GuiSvcLib')
+    env.Tool('GlastSvcLib')
     env.Tool('TkrUtilLib')
+    env.Tool('addLibrary', library = env['gaudiLibs'])
+    env.Tool('addLibrary', library = env['clhepLibs'])
 def exists(env):
     return 1;
